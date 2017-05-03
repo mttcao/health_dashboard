a7bf6398d07a57220df0d9727701c6fab19ccfa3
tya.cao <tya.cao@myob.com>
2017-05-03 21:27:49 +1000

    Add state for ApiStatus

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 // import {Ajax} from 'react-superagent';
-import { getAll } from './APIService';
+import {getAll} from './APIService';
+import './App.css';
+
+const initialState = {
+  apiStatus: 0,
+
+};
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {apiStatus: 0};
-    }
-    render() {
-        return (
-            <div>
-                "test:" {this.state.apiStatus}
-            </div>
-        );
-    }
-}
+  constructor(props) {
+    super(props);
+    this.state = initialState;
+  };
 
-function test() {
-    getAll().then(function (res) {
-        this.setState({apiStatus: res.status})
-    });
+  test = () => {
+    var self = this;
+    getAll().then((result) => {
+      console.log(result.status)
+      self.setState({
+        apiStatus: result.status
+      })
+    }).catch((error) => {
+      console.log(error.status)
+      self.setState({
+        apiStatus: error.status
+      });
+    })
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.test(),
+      10000
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        "Test status:" {this.state.apiStatus}
+      </div>
+    );
+  };
 }
 
 export default App;
